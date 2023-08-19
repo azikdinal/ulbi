c7c2f6c5fa997ac07539ccab788484a92916581e
azamat <dinaev2002@gmail.com>
2023-08-19 11:01:00 +0300

    1.4 lesson completed

---
+++ src/components/Counter.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import "./Counter.scss"
+export const Counter = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>increment</button>
+            <h1>{count}</h1>
+        </div>
+    );
+};
