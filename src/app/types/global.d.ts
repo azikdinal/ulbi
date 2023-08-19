83b156e1083f5bc25830d4c3abcdfaefe9f912a6
azamat <dinaev2002@gmail.com>
2023-08-19 19:54:28 +0300

    2.4 lesson is completed

---
+++ src/app/types/global.d.ts
@@ -0,0 +1,17 @@
+declare module '*.scss' {
+    interface IClassNames {
+        [className: string]: string
+    }
+    const classNames: IClassNames;
+    export = classNames;
+}
+
+declare module "*.png"
+declare module "*.jpg"
+declare module "*.jpeg"
+
+declare module "*.svg" {
+    import {VFC} from "react"
+    const SVG: VFC<React.SVGProps<SVGSVGElement>>
+    export default SVG
+}
